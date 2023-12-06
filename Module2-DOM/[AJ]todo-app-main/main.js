704048d3a07e158382cb6e39d2cd7e9600a1f3d0
ssarunyu <ssarunyu04@gmail.com>
2023-12-06 22:09:22 +0700

    commit

--- Module2-DOM/[AJ]todo-app-main/main.js
+++ Module2-DOM/[AJ]todo-app-main/main.js
@@ -11,10 +11,10 @@ window.addEventListener('beforeunload', (event) => {
   beforeUnloadHandler(event)
 })
 
-const addButton = document.getElementById('addBtn')
-addButton.addEventListener('click', () => addTodoHandler())
+// const addButton = document.getElementById('addBtn')
+// addButton.addEventListener('click', () => addTodoHandler())
 
-const inputTodo = document.querySelector('input')
-inputTodo.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') addTodoHandler()
-})
+// const inputTodo = document.querySelector('input')
+// inputTodo.addEventListener('keypress', (event) => {
+//   if (event.key === 'Enter') addTodoHandler()
+// })
