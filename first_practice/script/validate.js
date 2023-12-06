8cf7bd115201be5192a2d7e113e417ec35fc4558
ssarunyu <ssarunyu04@gmail.com>
2023-12-06 21:13:06 +0700

    commit

---
+++ first_practice/script/validate.js
@@ -0,0 +1,11 @@
+const validateUsername = (username) => {
+}
+
+const validateEmail = (email) => {
+}
+
+const validatePassword = (password) => {
+}
+
+
+export { validateEmail, validatePassword, validateUsername }
